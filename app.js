e08886663130aa80463e487bbbe931d5cc7afe7c
Tony Stark <tonystark@Jarvis.local>
2017-12-11 07:15:25 +0300

    created package.json and app.js

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+   // session.send("You said: %s", session.message.text);
     session.send("Hi there, nice to meet you. I am your campus bot, you can call me Jarvis.")
 });
